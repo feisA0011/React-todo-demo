02882fa3ce3f22182d3dcd70f1887bde909ee7f7
Feisal Mohamud Ahmed <6140236@RTS-365.dk>
2020-04-01 21:05:23 +0200

    Updates

---
+++ precache-manifest.c6839fc44d4dee3251729959d4b7275a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7260570a5884dfd643ce7b6cbe52226",
+    "url": "/React-todo-demo/index.html"
+  },
+  {
+    "revision": "81a997a3bd9fc3b08621",
+    "url": "/React-todo-demo/static/css/main.55405c2c.chunk.css"
+  },
+  {
+    "revision": "f66861b797a6fbc8eaeb",
+    "url": "/React-todo-demo/static/js/2.c82daa48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-demo/static/js/2.c82daa48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a997a3bd9fc3b08621",
+    "url": "/React-todo-demo/static/js/main.967c7de3.chunk.js"
+  },
+  {
+    "revision": "1c0cef6d3c9a1d3f0234",
+    "url": "/React-todo-demo/static/js/runtime-main.703b94aa.js"
+  }
+]);
